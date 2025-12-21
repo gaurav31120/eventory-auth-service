d4289d853149399efac800257647c56b69ea8b9c
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-21 13:47:48 +0530

    prettier code formatted

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 console.log('Welcome to eventory -- auth service')
 
-function welcome(name: string){
+function welcome(name: string) {
     console.log('Welcome, ' + name)
 }
 
-welcome('Gaurav');
+welcome('Gaurav')
